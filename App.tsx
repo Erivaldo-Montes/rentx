1055e76b19f4dda66472515efb721f112c9773da
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-10-17 22:10:56 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "react-native";
+import { Login } from "./src/screens/Login";
+import { ThemeProvider } from "styled-components/native";
+import { theme } from "./src/theme";
+import {
+  useFonts,
+  Archivo_700Bold,
+  Archivo_600SemiBold,
+} from "@expo-google-fonts/archivo";
+import {
+  Inter_400Regular,
+  Inter_700Bold,
+  Inter_500Medium,
+} from "@expo-google-fonts/inter";
 
 export default function App() {
+  const [fontsLoaded, fontsError] = useFonts({
+    Inter_400Regular,
+    Inter_700Bold,
+    Inter_500Medium,
+    Archivo_700Bold,
+    Archivo_600SemiBold,
+  });
+
+  if (!fontsLoaded && !fontsError) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle={"dark-content"}
+        backgroundColor={"transparent"}
+        translucent
+      />
+      <Login />
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
