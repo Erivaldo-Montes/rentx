165b81ee185318c51968707389dddd3ee538702b
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-10-21 22:03:21 -0300

    disable button

--- src/screens/SignUp/SignUpSecondStep/index.tsx
+++ src/screens/SignUp/SignUpSecondStep/index.tsx
@@ -6,21 +6,29 @@ import { SignUpHeader } from "@/components/SignUpHeader";
 import { Input } from "@/components/Input";
 import { Button } from "@/components/Button";
 import { Container, Form, FormTitle, ErrorMessageText } from "./styles";
+import { useEffect, useState } from "react";
 
 const confirmPasswordSchema = yup.object({
-  password: yup.string().required("Insira a senha"),
+  password: yup
+    .string()
+    .required("Insira a senha")
+    .min(8, "A senha de ter no mínimo 8 caracteres"),
   confirmPassword: yup
     .string()
     .required("Insira a confirmação da senha")
+
     .oneOf([yup.ref("password")], "As senhas não são iguais"),
 });
 
 type confirmPasswordData = yup.InferType<typeof confirmPasswordSchema>;
 
 export function SignUpSecondStep() {
+  const [isDisableButton, setIsDisableButton] = useState(true);
+
   const {
     control,
     handleSubmit,
+    watch,
     formState: { errors },
   } = useForm<confirmPasswordData>({
     resolver: yupResolver(confirmPasswordSchema),
@@ -29,6 +37,24 @@ export function SignUpSecondStep() {
   function handleRegister(data: confirmPasswordData) {
     console.log(data);
   }
+
+  const watchPassword = watch("password");
+  const watchConfirmPassword = watch("confirmPassword");
+
+  useEffect(() => {
+    setIsDisableButton(() => {
+      if (!watchConfirmPassword || !watchPassword) {
+        return true;
+      }
+      if (errors.confirmPassword?.message || errors.password?.message) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+    console.log(isDisableButton);
+  }, [watchConfirmPassword, watchPassword, errors]);
+
   return (
     <Container>
       <SignUpHeader stepsActive={2} />
@@ -74,8 +100,9 @@ export function SignUpSecondStep() {
         />
       </Form>
       <Button
-        styleButtom="RED"
-        title="Próximo"
+        styleButtom="GREEN"
+        title="Cadastrar"
+        disabled={isDisableButton}
         onPress={handleSubmit(handleRegister)}
       />
     </Container>
