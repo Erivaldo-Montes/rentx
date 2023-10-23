65047266b35ac5596b401e60cc81b09e3d2e0d03
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-10-23 18:48:09 -0300

    stack navigation routes

--- src/routes/app.route.tsx
+++ src/routes/app.route.tsx
@@ -10,10 +10,10 @@ const { Navigator, Screen } = createNativeStackNavigator();
 export function AppRoute() {
   return (
     <Navigator screenOptions={{ headerShown: false }}>
-      <Screen name="signUpSecondStep" component={SignUpSecondStep} />
-      <Screen name="confirmScreen" component={ConfirmScreen} />
       <Screen name="signIn" component={SignIn} />
       <Screen name="signUpFirstStep" component={SignUpFirstStep} />
+      <Screen name="signUpSecondStep" component={SignUpSecondStep} />
+      <Screen name="confirmScreen" component={ConfirmScreen} />
     </Navigator>
   );
 }
