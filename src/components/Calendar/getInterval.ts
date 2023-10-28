e7a45fc842d2fdc7ed4f3402d4de157bb6e37dc2
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-10-28 20:38:09 -0300

    generate interval dates

---
+++ src/components/Calendar/getInterval.ts
@@ -0,0 +1,35 @@
+import { eachDayOfInterval, isEqual, format, addDays } from "date-fns";
+import { theme } from "@/theme/index";
+import { useState } from "react";
+import { MarkedDateProps, DayProps } from "./index";
+
+interface Props {
+  startDate: DayProps;
+  endDate: DayProps;
+}
+
+export function getInterval({ startDate, endDate }: Props) {
+  const dataFnsInterval = eachDayOfInterval({
+    start: new Date(startDate.timestamp),
+    end: new Date(endDate.timestamp),
+  });
+  let markedDates: MarkedDateProps = {};
+  dataFnsInterval.map((date) => {
+    const dateKey = format(addDays(date, 1), "yyyy-MM-dd");
+    markedDates = {
+      ...markedDates,
+      [dateKey]: {
+        textColor:
+          startDate.dateString === dateKey || endDate.dateString === dateKey
+            ? theme.COLORS.white
+            : theme.COLORS["gray-500"],
+        color:
+          startDate.dateString === dateKey || endDate.dateString === dateKey
+            ? theme.COLORS["red-600"]
+            : theme.COLORS["red-100"],
+      },
+    };
+  });
+
+  return markedDates;
+}
