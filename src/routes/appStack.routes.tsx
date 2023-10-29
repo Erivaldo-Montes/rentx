e7c63752a9f417021049a0f133f6a5e98b76fa7c
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-10-29 16:28:47 -0300

    routes navigation

--- src/routes/appStack.routes.tsx
+++ src/routes/appStack.routes.tsx
@@ -1,7 +1,10 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { CarDetails } from "@/screens/CarDetails";
-import { Home } from "@/screens/Home";
 import { AppRoute } from "./app.routes";
+import { Scheduling } from "@/screens/Scheduling";
+import { ResumeRent } from "@/screens/ResumeRent";
+import { ConfirmScreen } from "@/screens/ConfirmScreen";
+
 const { Navigator, Screen } = createNativeStackNavigator();
 
 export function AppStack() {
@@ -13,6 +16,9 @@ export function AppStack() {
     >
       <Screen name="Home" component={AppRoute} />
       <Screen name="CarDetails" component={CarDetails} />
+      <Screen name="Scheduling" component={Scheduling} />
+      <Screen name="ResumeRent" component={ResumeRent} />
+      <Screen name="ConfirmScreen" component={ConfirmScreen} />
     </Navigator>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,12 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { AppRoute } from "./app.route";
+import { AppRoute } from "./app.routes";
 import { AuthRoute } from "./auth.routes";
+import { AppStack } from "./appStack.routes";
 
 export function Routes() {
   return (
     <NavigationContainer>
-      <AuthRoute />
+      <AppStack />
     </NavigationContainer>
   );
 }
