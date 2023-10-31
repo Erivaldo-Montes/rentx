1e2280cae073af008b81554a6deccbaf25d3af62
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-10-30 21:12:51 -0300

    fix to 2 decimals

--- src/utils/metrics.ts
+++ src/utils/metrics.ts
@@ -5,9 +5,17 @@ const { width, height } = Dimensions.get("window");
 const guidelineBaseWidth = 375;
 const guidelineBaseHeight = 812;
 
-const horizontalScale = (size: number) => (width / guidelineBaseWidth) * size;
-const verticalScale = (size: number) => (height / guidelineBaseHeight) * size;
-const moderateScale = (size: number, factor = 0.5) =>
-  size + (horizontalScale(size) - size) * factor;
+const horizontalScale = (size: number) => {
+  const result: number = (width / guidelineBaseWidth) * size;
+  return Number(result.toFixed(2));
+};
+const verticalScale = (size: number) => {
+  const result = (height / guidelineBaseHeight) * size;
+  return Number(result.toFixed(2));
+};
 
+const moderateScale = (size: number, factor = 0.5) => {
+  const result = size + (horizontalScale(size) - size) * factor;
+  return Number(result.toFixed(2));
+};
 export { horizontalScale, verticalScale, moderateScale };
