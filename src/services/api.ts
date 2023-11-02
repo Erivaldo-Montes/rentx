d8db418665b9227e8a5d644d065d0a96013725cd
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-11-02 18:58:22 -0300

    axios intercept

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,17 @@
 import Axios from "axios";
+import { AppError } from "@/utils/AppError";
 
-export const axios = Axios.create({
+const axios = Axios.create({
   baseURL: "http://10.0.0.104:3333",
 });
+
+axios.interceptors.response.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    new AppError(error);
+  }
+);
+
+export { axios };
